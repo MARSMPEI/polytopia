17eb453433724db296dff59c7dbb1a4d28dda1da
MARS <61511540+MARSMPEI@users.noreply.github.com>
2022-10-30 23:45:10 +0300

    detect positoin of block by ray

--- Scripts/RayScript.cs
+++ Scripts/RayScript.cs
@@ -6,20 +6,27 @@ public class RayScript : MonoBehaviour
 {
     public Transform Pointer;
     Ray ray = new Ray();
-    public float distance;
+  //  public float distance;
     private void LateUpdate()
     {
         ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        Debug.DrawRay(transform.position, transform.forward * 10f, Color.yellow);
+        Debug.DrawRay(transform.position, transform.forward * 100f, Color.yellow);
         //  ray.origin = transform.position;
         //Vector3 directioinOfRay = new Vector3(1, -1, 0);
 
        
      
-        if (Physics.Raycast(ray, distance) )
+        if (Physics.Raycast(ray , out RaycastHit hitInfo) )
         {
-      
-            
+            /*
+            Debug.Log("hit normal " + hitInfo.normal);
+            Debug.Log("hit distance " +(int)hitInfo.distance);
+            Debug.Log("hit textureCoord2 " + hitInfo.textureCoord2);
+            Debug.Log("hit textureCoord " + hitInfo.textureCoord);
+            */
+            // Debug.Log("hit barycentricCoordinate " + hitInfo.barycentricCoordinate);
+         //   Debug.Log("hit collider " + hitInfo.collider); 
+            Debug.Log("hit transform " + hitInfo.transform.position);
 
         }
         
